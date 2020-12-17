345847a6b3c1d71766bd96f5879f5dc3cb2d75bf
Isvvc <lyons@tuta.io>
2020-12-17 13:40:38 -0700

    Create simple editable image link list

--- Armor Export/src/Armor_Export.py
+++ Armor Export/src/Armor_Export.py
@@ -1,10 +1,14 @@
 import PySimpleGUI as sg
 
+images = {"https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png"}
+
 # Define the window's contents
-layout = [[sg.Text("What's your name?")],
-          [sg.Input(key='-INPUT-')],
-          [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Button('OK'), sg.Button('Quit')]]
+layout = [[sg.Text("Image URL")],
+          [sg.Input(key='-INPUT-'), sg.Button('Add')],
+          [sg.Text('Images')],
+          [sg.Listbox(list(images), size=(40,10), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, key='-LIST-')],
+          [sg.Button('Remove selected', key='Remove')],
+          [sg.Button('Quit')]]
 
 # Create the window
 window = sg.Window('Window Title', layout)
@@ -12,11 +16,17 @@ window = sg.Window('Window Title', layout)
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
+
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
-    # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
+    elif event == 'Add':
+        images.add(values['-INPUT-'])
+    elif event == 'Remove':
+        [images.remove(x) for x in values['-LIST-']]
+
+    window['-LIST-'].update(list(images))
 
 # Finish up by removing from the screen
 window.close()
