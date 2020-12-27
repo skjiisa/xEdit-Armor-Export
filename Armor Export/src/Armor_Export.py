9ee95dcf4dba1c8e3ccd52f90dfc2f6c614044ca
Isaac Lyons <lyons@tuta.io>
2020-12-27 15:59:45 -0700

    Create image preview window

--- Armor Export/src/Armor_Export.py
+++ Armor Export/src/Armor_Export.py
@@ -24,17 +24,27 @@ def image_data(url: str) -> bytes:
         img = img.resize((int(cur_width*scale), int(cur_height*scale)), Image.ANTIALIAS)
     
     bio = BytesIO()
-    img.save(bio, format="PNG")
+    img.save(bio, format='PNG')
     del img
     image_cache[url] = bio.getvalue()
     return image_cache[url]
+
+def make_images_window(images):
+    col = sg.Column([[
+        sg.Checkbox('', key=f'Checkbox{index}'),
+        sg.Image(data=image_data(image), enable_events=True, key=f'Image{index}')
+    ] for index, image in enumerate(images)], size=(500,1000), scrollable=True)
+    images_layout = [[col]]
+    new_window = sg.Window('Images', images_layout, finalize=True)
+    return new_window
 
 images_col = [
     [sg.Text('Images')],
     [sg.Listbox(images, size=(40,10), key='-LIST-')],
     [
         sg.Button('Remove selected', key='Remove'),
-        sg.Button('Preview selected', key='Preview')
+        sg.Button('Preview selected', key='Preview'),
+        sg.Button('Preview all', key='PreviewAll')
     ]
 ]
 
@@ -53,29 +63,43 @@ layout = [
     [sg.Button('Quit')]
 ]
 
-window = sg.Window('Armor Export', layout)
+window = sg.Window('Armor Export', layout, finalize=True)
+images_window = None
 
 while True:
-    event, values = window.read()
-
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
+    active_window, event, values = sg.read_all_windows()
+    print(event)
+    if (event == sg.WINDOW_CLOSED and active_window == window) or event == 'Quit':
         break
 
     elif event == 'Add':
         input = values['-INPUT-']
         if not input in images:
             images.append(input)
+            window['-LIST-'].update(images)
+    
     elif event == 'Remove':
         [images.remove(x) for x in values['-LIST-']]
+        window['-LIST-'].update(images)
+    
     elif event == 'Preview':
         selection = values['-LIST-']
         if len(selection) > 0:
             window['-PREVIEW-'].update(data=image_data(selection[0]))
+    
     elif event == 'PreviewInput':
         input = values['-INPUT-']
         window['-PREVIEW-'].update(data=image_data(input))
-
-    window['-LIST-'].update(images)
+    
+    elif event == 'PreviewAll':
+        images_window = make_images_window(images)
+        
+    elif event == sg.WINDOW_CLOSED and active_window == images_window:
+        images_window.close()
+        images_window = None
+        
+    elif "Image" in event:
+        index = event[5:]
+        images_window[f'Checkbox{index}'].update(not images_window[f'Checkbox{index}'].get())
 
 window.close()
-
