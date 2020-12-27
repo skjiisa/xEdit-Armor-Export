3a468e8a604d0d7509db4a0d513e932d9ab812ba
Isaac Lyons <lyons@tuta.io>
2020-12-27 13:01:56 -0700

    Create function to lead image data from URL
    Improve the layout array format
    Change images set to list
    Support Windows HiDPI

--- Armor Export/src/Armor_Export.py
+++ Armor Export/src/Armor_Export.py
@@ -1,14 +1,35 @@
 import PySimpleGUI as sg
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
 
-images = {"https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png"}
+# Support HiDPI
+import ctypes
+import platform
+if int(platform.release()) >= 8:
+    ctypes.windll.shcore.SetProcessDpiAwareness(True)
+
+def image_data(url: str) -> bytes:
+    data = BytesIO(requests.get(url).content)
+    
+    img = Image.open(data)
+    bio = BytesIO()
+    img.save(bio, format="PNG")
+    del img
+    return bio.getvalue()
+
+images = ["https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png"]
 
 # Define the window's contents
-layout = [[sg.Text("Image URL")],
-          [sg.Input(key='-INPUT-'), sg.Button('Add')],
-          [sg.Text('Images')],
-          [sg.Listbox(list(images), size=(40,10), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, key='-LIST-')],
-          [sg.Button('Remove selected', key='Remove')],
-          [sg.Button('Quit')]]
+layout = [
+    [sg.Text("Image URL")],
+    [sg.Input(key='-INPUT-'), sg.Button('Add')],
+    [sg.Text('Images')],
+    [sg.Listbox(images, size=(40,10), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, key='-LIST-')],
+    [sg.Button('Remove selected', key='Remove')],
+    #[sg.Image(data=image_data(images[0]))],
+    [sg.Button('Quit')]
+]
 
 # Create the window
 window = sg.Window('Window Title', layout)
@@ -22,11 +43,13 @@ while True:
         break
 
     elif event == 'Add':
-        images.add(values['-INPUT-'])
+        input = values['-INPUT-']
+        if not input in images:
+            images.append(input)
     elif event == 'Remove':
         [images.remove(x) for x in values['-LIST-']]
 
-    window['-LIST-'].update(list(images))
+    window['-LIST-'].update(images)
 
 # Finish up by removing from the screen
 window.close()
